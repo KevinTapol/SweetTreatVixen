8234eacd6a218fc8d7dc0001a612ad578f41fd46
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-11-19 14:07:21 -0600

    added pastry shop background upsplash to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import pizza from '/imgs/pizza_splash.jpg'
+import shop from '/imgs/shop.jpg'
 import { useInView } from 'react-intersection-observer'
 import chef from '/imgs/chef.jpg'
 
@@ -7,7 +7,7 @@ const Main = () => {
     const { ref:myRef, inView: myElementIsVisible } = useInView();
   return (
     <div>
-        <img className='w-full h-screen object-cover' src={pizza} alt="" />
+        <img className='w-full h-screen object-cover' src={shop} alt="" />
         <div className='w-full h-screen absolute top-0 left-0 bg-black/10 flex justify-center items-center'>
             <div ref={myRef} className={`${myElementIsVisible ? 'absolute hero_text' : 'absolute  opacity-0' }`}>
                 <h1 className='text-[4vw] font-bold text-black/50 text-center font-sans'>Sweet Treat Vixen</h1>
