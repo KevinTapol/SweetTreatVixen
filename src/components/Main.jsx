d351fadf5ee4a3d695742dd1c6fc2bedf7c6e363
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-11-19 07:26:58 -0600

    added hero pic rotation on Y axis in index.css key frame animations

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
         <img className='w-full h-screen object-cover' src={pizza} alt="" />
         <div className='w-full h-screen absolute top-0 left-0 bg-black/10 flex justify-center items-center'>
             <div ref={myRef} className={`${myElementIsVisible ? 'absolute hero_text' : 'absolute  opacity-0' }`}>
-                <h1 className='md:text-4xl text-xl font-bold text-black/50 text-center'>Sweet Treat Vixen</h1>
+                <h1 className='md:text-4xl text-xl font-bold text-black/50 text-center font-sans'>Sweet Treat Vixen</h1>
                 <img src={chef} className='w-[450px] opacity-90' alt="" />
             </div>
         </div>
