5c2ec90f4fe6335e203853d47b5fcd801b846ba4
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-11-24 13:13:39 -0600

    replaced useInView hook with useState useRef and useEffect

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,36 @@
+import React, { useState, useRef, useEffect } from 'react'
+import shop from '/imgs/shop.jpg'
+import chef from '/imgs/chef.jpg'
+
+const Main = () => {
+
+  const myRef = useRef();
+  const [myElementIsVisible, setMyElementIsVisible] = useState();
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries)=> {
+    const entry = entries[0];
+      setMyElementIsVisible(entry.isIntersecting)      
+    },{
+      threshold: 0
+    })
+    observer.observe(myRef.current)
+  }, [])
+
+  return (
+    <div id='home'>
+        <img className='w-full h-screen object-cover' src={shop} alt="" />
+        <div className='w-full h-screen absolute top-0 left-0 bg-black/10 flex justify-center items-center'>
+            <div ref={myRef} className={`${myElementIsVisible ? 'absolute hero_text' : 'absolute  opacity-0' }`}>
+                <h1 className='text-[4vw] font-bold text-white text-center banner'>Sweet Treat Vixen</h1>
+                <img src={chef} className='block m-auto w-[40%] opacity-90 rounded-[15%]' alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Main
+
+/*
 import React from 'react'
 import shop from '/imgs/shop.jpg'
 import { useInView } from 'react-intersection-observer'
@@ -18,3 +51,4 @@ const Main = () => {
   )
 }
 export default Main
+*/
