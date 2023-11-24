af2708b9385e0f5c57651697b89ffa2571eaad46
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-11-24 15:49:46 -0600

    created new css key frame animation main_img to scale in and rotate on Y axis

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -74,8 +74,8 @@ const Main = () => {
     <div id='home'>
         <img className='w-full h-screen object-cover' src={shop} alt="" />
         <div className='w-full h-screen absolute top-0 left-0 bg-black/10 flex justify-center items-center'>
-            <div ref={myRef} className={`${myElementIsVisible ? 'absolute main_text' : 'absolute  opacity-0' }`}>
-                <h1 className='text-[4vw] font-bold text-white text-center banner'>Sweet Treat Vixen</h1>
+            <div ref={myRef} className={`${myElementIsVisible ? 'absolute main_img' : 'absolute  opacity-0' }`}>
+                <h1 className='text-[4vw] font-bold text-pink-500 text-center banner'>Sweet Treat Vixen</h1>
                 <img src={chef} className='block m-auto w-[40%] opacity-90 rounded-[15%]' alt="" />
             </div>
         </div>
