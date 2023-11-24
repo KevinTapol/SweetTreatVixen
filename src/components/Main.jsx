63f6398777b39d9f33f5d84bebfca9413dde1924
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-11-24 13:15:55 -0600

    changed main splash color and react hooks to test animation on apple devices

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -21,7 +21,7 @@ const Main = () => {
         <img className='w-full h-screen object-cover' src={shop} alt="" />
         <div className='w-full h-screen absolute top-0 left-0 bg-black/10 flex justify-center items-center'>
             <div ref={myRef} className={`${myElementIsVisible ? 'absolute hero_text' : 'absolute  opacity-0' }`}>
-                <h1 className='text-[4vw] font-bold text-white text-center banner'>Sweet Treat Vixen</h1>
+                <h1 className='text-[4vw] font-bold text-purple-400 text-center banner'>Sweet Treat Vixen</h1>
                 <img src={chef} className='block m-auto w-[40%] opacity-90 rounded-[15%]' alt="" />
             </div>
         </div>
