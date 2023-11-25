e824eb42b223f348d3770aa05c7a033d9e330a8b
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-11-24 20:54:46 -0600

    added more realistic image of client

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -66,13 +66,13 @@ export default Main
 import React from 'react'
 import shop from '/imgs/shop.jpg'
 import { useInView } from 'react-intersection-observer'
-import chef from '/imgs/chef.jpg'
+import chef from '/imgs/pastry_chef.jpg'
 
 const Main = () => {
     const { ref:myRef, inView: myElementIsVisible } = useInView();
   return (
     <div id='home'>
-        <img className='w-full h-screen object-cover' src={shop} alt="" />
+        <img className='w-full h-screen object-cover opacity-70' src={shop} alt="" />
         <div className='w-full h-screen absolute top-0 left-0 bg-black/10 flex justify-center items-center'>
             <div ref={myRef} className={`${myElementIsVisible ? 'absolute main_img' : 'absolute  opacity-0 ' }`}>
                 <h1 className='text-[4vw] font-bold text-pink-500 text-center font_pacifico'>Sweet Treat Vixen</h1>
