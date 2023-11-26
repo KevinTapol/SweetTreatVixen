187cba506de5084c209b967aedbd743d7f147cd5
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-11-26 08:47:10 -0600

    adjusted the main text size for responsiveness

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -75,7 +75,7 @@ const Main = () => {
         <img className='w-full h-screen object-cover opacity-70' src={shop} alt="" />
         <div className='w-full h-screen absolute top-0 left-0 bg-black/10 flex justify-center items-center'>
             <div ref={myRef} className={`${myElementIsVisible ? 'absolute main_img' : 'absolute  opacity-0 ' }`}>
-                <h1 className='text-[6vw] font-bold text-pink-500 text-center font_pacifico'>Sweet Treat Vixen</h1>
+                <h1 className='text-[5vw] font-bold text-pink-500 text-center font_pacifico'>Sweet Treat Vixen</h1>
                 <img src={chef} className='block m-auto w-1/2 opacity-90 rounded-[100%]' alt="" />
             </div>
         </div>
