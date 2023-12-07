a20d5d7fd956488f04d638981c63118dd0ad05eb
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-12-06 18:32:40 -0600

    added navbar for larger screens with md:block hidden and md:hidden

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,11 @@ const Navbar = () => {
     }
   return (
     <div id='navbar' className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white absolute'>
-    
+    {/* make this mobile nav hidden at screen sizes medium and above then create a new nav for medium and above named Catering Menu instead of icon. Maybe just changed the icon to be an h2 Catering Icon and get rid of the icon entirely? */}
         {/* hamburger menu useState logic for open close to display hamburger or X */}
         <div onClick={handleNav} className='flex flex-end text-pink-500 top-2 right-2 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer '  >
-            {nav ? <AiOutlineClose className='text-red-500' size={30} /> : <AiOutlineMenu size={30} className=''/>}
+            {nav ? <AiOutlineClose className='md:hidden text-red-500' size={30} /> : <AiOutlineMenu size={30} className='md:hidden'/> }
+            {nav ? <AiOutlineClose className='md:block hidden text-red-500' size={30} /> : <h2 className='md:block hidden border-2 border-pink-500 p-2 rounded-lg bg-black/60 font_pacifico m-4 font-bold'>Catering Menu</h2>}
         </div>
         {/* menu is off the page by 100% to the left and pulled on by 80% when hamburger cliked */}
         <div className={nav ? 'fixed left-0 top-0 w-[80%] h-full border-r border-r-gray-900 bg-[#000300]/80 ease-in-out duration-500 z-[999] md:w-[35%]' : 'fixed left-[-100%] z-[999]'} >
