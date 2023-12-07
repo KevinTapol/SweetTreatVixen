a394f21405d25fc7c4319e4000b5f89bdd58a407
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-12-07 16:18:35 -0600

    changed styles on navbar and refactored modal code

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
 
-const Modal = ({name, imgUrl, bread, ingredients, directions, open, onClose, instructions, small, medium, large}) => {
+const Modal = ({name, imgUrl, ingredients, directions, open, onClose, instructions, small, medium, large}) => {
     if (!open) return null
   return (
     <div className='fixed bg-black/60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center' onClick={onClose}>
@@ -9,24 +9,17 @@ const Modal = ({name, imgUrl, bread, ingredients, directions, open, onClose, ins
             <AiOutlineClose className='absolute right-6 top-6 text-3xl text-red-600 cursor-pointer hover:scale-[120%] ease-in duration-200' onClick={onClose}/>
             <h2 className='w-fit px-4 py-1 bg-pink-500 rounded-lg font_pacifico'>{name}</h2>
             <div className='flex justify-center items-center gap-x-2'>
-                <h2 className='my-1 font_pacifico text-pink-500'>{bread}{directions}</h2>
+                <h2 className='my-1 font_pacifico text-pink-500'>{directions}</h2>
             </div>
             <img className='block mx-auto w-[30%] h-[30%] object-cover' src={imgUrl} alt="" />
             <h2 className='flex justify-center m-2 font_pacifico text-pink-500'>{instructions}</h2>
-            {/* <div className='flex justify-center'>
-                <h2 className='flex justify-center m-2 font_pacifico text-pink-500'>Size</h2>
-                <h2 className='flex justify-center m-2 font_pacifico text-pink-500'>Price</h2>
-            </div> */}
             <div className='flex justify-center'>
-                {/* <h2 className='flex justify-center m-2 font_pacifico text-pink-500'>Small (feeds 6-8)</h2> */}
                 <h2 className='flex justify-center m-2 font_pacifico text-pink-500'>{small}</h2>
             </div>
             <div className='flex justify-center'>
-                {/* <h2 className='flex justify-center m-2 font_pacifico text-pink-500'>Medium (feeds 8-10)</h2> */}
                 <h2 className='flex justify-center m-2 font_pacifico text-pink-500'>{medium}</h2>
             </div>
             <div className='flex justify-center'>
-                {/* <h2 className='flex justify-center m-2 font_pacifico text-pink-500'>Large (feeds 10-12)</h2> */}
                 <h2 className='flex justify-center m-2 font_pacifico text-pink-500'>{large}</h2>
             </div>
         </div>  
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,21 +13,23 @@ const Navbar = () => {
     {/* make this mobile nav hidden at screen sizes medium and above then create a new nav for medium and above named Catering Menu instead of icon. Maybe just changed the icon to be an h2 Catering Icon and get rid of the icon entirely? */}
         {/* hamburger menu useState logic for open close to display hamburger or X */}
         <div onClick={handleNav} className='flex flex-end text-pink-500 top-2 right-2 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer '  >
-            {nav ? <AiOutlineClose className='md:hidden text-red-500' size={30} /> : <AiOutlineMenu size={30} className='md:hidden'/> }
-            {nav ? <AiOutlineClose className='md:block hidden text-red-500' size={30} /> : <h2 className='md:block hidden border-2 border-pink-500 p-2 rounded-lg bg-black/60 font_pacifico m-4 font-bold'>Catering Menu</h2>}
+            {/* mobile navbar */}
+            {nav ? <AiOutlineClose className='md:hidden text-pink-500' size={30} /> : <AiOutlineMenu size={30} className='md:hidden'/> }
+            {/* screen size 786px and above navbar */}
+            {nav ? <AiOutlineClose className='md:block hidden text-pink-500 m-4' size={30} /> : <h2 className='md:block hidden border-2 border-pink-500 p-2 rounded-lg bg-black/60 font_pacifico m-4'>Catering Menu</h2>}
         </div>
-        {/* menu is off the page by 100% to the left and pulled on by 80% when hamburger cliked */}
+        {/* menu is off the page by 100% to the left and pulled on by 80% when hamburger clicked */}
         <div className={nav ? 'fixed left-0 top-0 w-[80%] h-full border-r border-r-gray-900 bg-[#000300]/80 ease-in-out duration-500 z-[999] md:w-[35%]' : 'fixed left-[-100%] z-[999]'} >
-            <h1 className='w-full text-3xl font-bold text-pink-500 font_pacifico text-center mt-4'>Catering Menu</h1>
+            <h1 className='w-full text-3xl font-bold text-pink-500 font_pacifico text-center my-4'>Catering Menu</h1>
             {/* create a template for array of items in the json as anchor tags adding href by item id */}
             <div className='flex flex-col'>
                 <div className='block m-auto'>
                 {dataArray.map((item, index) => (
-                    <a onClick={handleNav} className='text-pink-500 font_pacifico flex justify-start hover:scale-[120%] ease-in duration-200' key={index} href={`#${item.id}`}>{item.name}</a>
+                    <a onClick={handleNav} className='text-pink-500 font_pacifico flex justify-start my-1 hover:scale-[120%] ease-in duration-200' key={index} href={`#${item.id}`}>{item.name}</a>
                 ))}
                 </div>
                 <a onClick={handleNav} href="#home" className='text-pink-500 font_pacifico mx-auto mt-4 hover:scale-[120%] ease-in duration-200'>Home</a>
-                <a onClick={handleNav} href="#contact" className='text-pink-500 font_pacifico mx-auto hover:scale-[120%] ease-in duration-200'>Contact</a>
+                <a onClick={handleNav} href="#contact" className='text-pink-500 font_pacifico mx-auto my-2 hover:scale-[120%] ease-in duration-200'>Contact</a>
             </div>
         </div>
     </div>
