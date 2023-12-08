7cf968a771e7cf05d664fd396b26af7af1846866
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-12-08 16:28:39 -0600

    undecided where to put shopping cart icon in navbar.jsx maybe put menu on the far left and cart on the far right

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 import dataArray from '../data/items.json'
+import { BsCart3 } from "react-icons/bs";
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
 
+    const [cart, setCart] = useState(false) 
+
     const handleNav = () => {
         setNav(!nav)
+    }
+
+    const handleCart = () => {
+        setCart(!cart)
     }
   return (
     <div id='navbar' className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white absolute'>
     {/* make this mobile nav hidden at screen sizes medium and above then create a new nav for medium and above named Catering Menu instead of icon. Maybe just changed the icon to be an h2 Catering Icon and get rid of the icon entirely? */}
         {/* hamburger menu useState logic for open close to display hamburger or X */}
-        <div onClick={handleNav} className='flex flex-end text-pink-500 top-2 right-2 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer '  >
+        <div onClick={handleNav} className='flex flex-end text-pink-500 top-2 right-2 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer'  >
             {/* mobile navbar */}
             {nav ? <AiOutlineClose className='md:hidden text-pink-500' size={30} /> : <AiOutlineMenu size={30} className='md:hidden'/> }
             {/* screen size 786px and above navbar */}
@@ -31,6 +38,16 @@ const Navbar = () => {
                 <a onClick={handleNav} href="#home" className='text-pink-500 font_pacifico mx-auto mt-4 hover:scale-[120%] ease-in duration-200'>Home</a>
                 <a onClick={handleNav} href="#contact" className='text-pink-500 font_pacifico mx-auto my-2 hover:scale-[120%] ease-in duration-200'>Contact</a>
             </div>
+            <div className={cart ? 'fixed right-0 top-0 w-[80%] h-full border-r border-r-gray-900 bg-[#000300]/80 ease-in-out duration-500 z-[999] md:w-[35%]' : 'fixed right-[-100%] z-[999]'}>
+                <h2 className='w-full text-3xl font-bold text-pink-500 font_pacifico text-center my-4'>Items in Cart</h2>
+                
+            </div>
+        </div>
+        <div onClick={handleCart} className=' hidden flex flex-end text-pink-500 top-2 right-4 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer '  >
+            {/* mobile navbar */}
+            {cart ? <AiOutlineClose className='md:hidden text-pink-500' size={30} /> : <BsCart3 size={30} className='md:hidden'/> }
+            {/* screen size 786px and above navbar */}
+            {cart ? <AiOutlineClose className='md:block hidden text-pink-500 m-4' size={30} /> : <h2 className='md:block hidden border-2 border-pink-500 p-2 rounded-lg bg-black/60 font_pacifico m-4'>Items Selected</h2>}
         </div>
     </div>
   )
