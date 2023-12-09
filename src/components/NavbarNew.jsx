c10a6de7af897a9ffd4914927e68e73c79a819ba
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2023-12-09 10:12:44 -0600

    moved catering menu to left side and added a cart menu component to right side

---
+++ src/components/NavbarNew.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import dataArray from '../data/items.json'
+import { BsCart3 } from "react-icons/bs";
+
+const NavbarNew = () => {
+    const [nav, setNav] = useState(false)
+
+    const [cart, setCart] = useState(false) 
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+    const handleCart = () => {
+        setCart(!cart)
+    }
+  return (
+    <div id='navbar' className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white absolute'>
+    {/* make this mobile nav hidden at screen sizes medium and above then create a new nav for medium and above named Catering Menu instead of icon. Maybe just changed the icon to be an h2 Catering Icon and get rid of the icon entirely? */}
+        {/* hamburger menu useState logic for open close to display hamburger or X */}
+        <div onClick={handleNav} className='flex flex-end text-pink-500 top-2 left-2 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer'  >
+            {/* mobile navbar */}
+            {nav ? <AiOutlineClose className='hidden text-pink-500' size={30} /> : <AiOutlineMenu size={30} className='md:hidden'/> }
+            {/* screen size 786px and above navbar */}
+            {nav ? <AiOutlineClose className='hidden text-pink-500 m-4' size={30} /> : <h2 className='md:block hidden border-2 border-pink-500 p-2 rounded-lg bg-black/60 font_pacifico m-4'>Catering Menu</h2>}
+        </div>
+        {/* menu is off the page by 100% to the left and pulled on by 80% when hamburger clicked */}
+        <div className={nav ? 'fixed left-0 top-0 w-[80%] h-full border-r border-r-gray-900 bg-[#000300]/80 ease-in-out duration-500 z-[999] md:w-[35%]' : 'fixed left-[-100%] z-[999]'}>
+            <div>
+                <div className='flex justify-end m-4'>
+                    <AiOutlineClose onClick={handleNav} size={30} className='text-pink-500 border-2 border-pink-500 rounded-lg hover:scale-[120%] ease-in duration-200'/>
+                </div>
+                <h1 className='w-full text-3xl font-bold text-pink-500 font_pacifico text-center my-4'>Catering Menu</h1>
+                {/* create a template for array of items in the json as anchor tags adding href by item id */}
+                <div className='flex flex-col'>
+                    <div className='block m-auto'>
+                    {dataArray.map((item, index) => (
+                        <a onClick={handleNav} className='text-pink-500 font_pacifico flex justify-start my-1 hover:scale-[120%] ease-in duration-200' key={index} href={`#${item.id}`}>{item.name}</a>
+                    ))}
+                    </div>
+                    <a onClick={handleNav} href="#home" className='text-pink-500 font_pacifico mx-auto mt-4 hover:scale-[120%] ease-in duration-200'>Home</a>
+                    <a onClick={handleNav} href="#contact" className='text-pink-500 font_pacifico mx-auto my-2 hover:scale-[120%] ease-in duration-200'>Contact</a>
+                </div>
+            </div>
+        </div>
+
+
+        <div onClick={handleCart} className=' flex flex-end text-pink-500 top-2 right-4 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer '  >
+            {/* mobile navbar */}
+            {cart ? <AiOutlineClose className='md:hidden text-pink-500' size={30} /> : <BsCart3 size={30} className='md:hidden'/> }
+            {/* screen size 786px and above navbar */}
+            {cart ? <AiOutlineClose className='md:block hidden text-pink-500 m-4' size={30} /> : <h2 className='md:block hidden border-2 border-pink-500 p-2 rounded-lg bg-black/60 font_pacifico m-4'>Items Selected</h2>}
+        </div>
+    </div>
+  )
+}
+
+export default NavbarNew
--- src/components/OldNavbar.jsx
+++ src/components/OldNavbar.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 import dataArray from '../data/items.json'
 import { BsCart3 } from "react-icons/bs";
+const OldNavbar = () => {
 
-const Navbar = () => {
     const [nav, setNav] = useState(false)
 
     const [cart, setCart] = useState(false) 
@@ -19,18 +19,15 @@ const Navbar = () => {
     <div id='navbar' className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white absolute'>
     {/* make this mobile nav hidden at screen sizes medium and above then create a new nav for medium and above named Catering Menu instead of icon. Maybe just changed the icon to be an h2 Catering Icon and get rid of the icon entirely? */}
         {/* hamburger menu useState logic for open close to display hamburger or X */}
-        <div onClick={handleNav} className='flex flex-end text-pink-500 top-2 left-2 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer'  >
+        <div onClick={handleNav} className='flex flex-end text-pink-500 top-2 right-2 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer'  >
             {/* mobile navbar */}
-            {nav ? <AiOutlineClose className='hidden text-pink-500' size={30} /> : <AiOutlineMenu size={30} className='md:hidden'/> }
+            {nav ? <AiOutlineClose className='md:hidden text-pink-500' size={30} /> : <AiOutlineMenu size={30} className='md:hidden'/> }
             {/* screen size 786px and above navbar */}
-            {nav ? <AiOutlineClose className='hidden text-pink-500 m-4' size={30} /> : <h2 className='md:block hidden border-2 border-pink-500 p-2 rounded-lg bg-black/60 font_pacifico m-4'>Catering Menu</h2>}
+            {nav ? <AiOutlineClose className='md:block hidden text-pink-500 m-4' size={30} /> : <h2 className='md:block hidden border-2 border-pink-500 p-2 rounded-lg bg-black/60 font_pacifico m-4'>Catering Menu</h2>}
         </div>
         {/* menu is off the page by 100% to the left and pulled on by 80% when hamburger clicked */}
         <div className={nav ? 'fixed left-0 top-0 w-[80%] h-full border-r border-r-gray-900 bg-[#000300]/80 ease-in-out duration-500 z-[999] md:w-[35%]' : 'fixed left-[-100%] z-[999]'} >
-            <div className='flex justify-end'>
-                <AiOutlineClose onClick={handleNav} size={30} className='text-pink-500 border-2 border-pink-500 rounded-lg m-4 hover:scale-[120%] ease-in duration-200'/>
-            </div>
-            <h1 className='w-full text-3xl font-bold text-pink-500 font_pacifico text-center mb-4'>Catering Menu</h1>
+            <h1 className='w-full text-3xl font-bold text-pink-500 font_pacifico text-center my-4'>Catering Menu</h1>
             {/* create a template for array of items in the json as anchor tags adding href by item id */}
             <div className='flex flex-col'>
                 <div className='block m-auto'>
@@ -42,21 +39,18 @@ const Navbar = () => {
                 <a onClick={handleNav} href="#contact" className='text-pink-500 font_pacifico mx-auto my-2 hover:scale-[120%] ease-in duration-200'>Contact</a>
             </div>
             <div className={cart ? 'fixed right-0 top-0 w-[80%] h-full border-r border-r-gray-900 bg-[#000300]/80 ease-in-out duration-500 z-[999] md:w-[35%]' : 'fixed right-[-100%] z-[999]'}>
-            <div className='flex justify-start'>
-                <AiOutlineClose onClick={handleCart} size={30} className='text-pink-500 border-2 border-pink-500 rounded-lg m-4 hover:scale-[120%] ease-in duration-200'/>
-            </div>
                 <h2 className='w-full text-3xl font-bold text-pink-500 font_pacifico text-center my-4'>Items in Cart</h2>
                 
             </div>
         </div>
-        <div onClick={handleCart} className='flex flex-end text-pink-500 top-2 right-4 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer '  >
+        <div onClick={handleCart} className=' hidden flex flex-end text-pink-500 top-2 right-4 z-[999] fixed hover:scale-[120%] ease-in duration-200 cursor-pointer '  >
             {/* mobile navbar */}
-            {cart ? <AiOutlineClose className='hidden text-pink-500' size={30} /> : <BsCart3 size={30} className='md:hidden'/> }
+            {cart ? <AiOutlineClose className='md:hidden text-pink-500' size={30} /> : <BsCart3 size={30} className='md:hidden'/> }
             {/* screen size 786px and above navbar */}
-            {cart ? <AiOutlineClose className='hidden text-pink-500 m-4' size={30} /> : <h2 className='md:block hidden border-2 border-pink-500 p-2 rounded-lg bg-black/60 font_pacifico m-4'>Items Selected</h2>}
+            {cart ? <AiOutlineClose className='md:block hidden text-pink-500 m-4' size={30} /> : <h2 className='md:block hidden border-2 border-pink-500 p-2 rounded-lg bg-black/60 font_pacifico m-4'>Items Selected</h2>}
         </div>
     </div>
   )
 }
 
-export default Navbar
+export default OldNavbar
