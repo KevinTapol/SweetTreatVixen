8e10dccb94d8e1cf27dbe16da6c349b585ac1c6b
Kevin Tapolcsanyi <kevintapolcanyi@gmail.com>
2024-01-29 18:09:38 -0600

    added new NavbarItems.jsx component to add a shopping cart and implement localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import NavbarNoCart from './components/NavbarNoCart'
 import Main2 from './components/Main2'
 import Contact3 from './components/Contact3'
 import Contact3b from './components/Contact3b'
+import NavbarNew from './components/NavbarNew'
 
 function App() {
   
